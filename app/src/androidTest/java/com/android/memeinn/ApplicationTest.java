751bf7d4289d249dd3e5d4502b96af26933c93dc
Chi Zhou <chriszhou1992@hotmail.com>
2015-02-17 20:09:38 -0600

    Rename packages

--- app/src/androidTest/java/com/android/memeinn/ApplicationTest.java
+++ app/src/androidTest/java/com/android/memeinn/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.richard.memeinn;
+package com.android.memeinn;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/android/memeinn/LoginActivity.java
+++ app/src/main/java/com/android/memeinn/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.richard.memeinn;
+package com.android.memeinn;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
