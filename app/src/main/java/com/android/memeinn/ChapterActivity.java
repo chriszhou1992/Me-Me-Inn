951fc68728e91c7af5d5b2a140541c0554889f8c
Lindsey Liu <jmjemi327@gmail.com>
2015-02-26 13:12:54 -0600

    Added Activity for Chapter list page

---
+++ app/src/main/java/com/android/memeinn/ChapterActivity.java
@@ -0,0 +1,16 @@
+package com.android.memeinn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lindsey on 15-02-26.
+ */
+public class ChapterActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chapterlist);
+    }
+}
