602e3eb603d7c354eb218f44b6fb315fececb569
Lindsey Liu <jmjemi327@gmail.com>
2015-03-04 19:42:39 -0600

    connect vocab and chapter list

--- app/src/main/java/com/android/memeinn/LoginActivity.java
+++ app/src/main/java/com/android/memeinn/LoginActivity.java
@@ -26,8 +26,8 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vocabpage);
-    /*
+        setContentView(R.layout.login);
+
         //Initialize Parse API to initialize connection to cloud
         Parse.initialize(this, "l5qhJIZRq3vPDrHTmyzPu3z6IwMjukw7M3h9A8CZ",
                 "iLgCs4Z7I71j1L9DIWrjwjkCZ02yc6KuDsYVO60e");
@@ -35,7 +35,6 @@ public class LoginActivity extends ActionBarActivity {
         //Fix the issue Android changes the font of password fields into monospace
         EditText passField = (EditText) findViewById(R.id.pword);
         passField.setTypeface(Typeface.DEFAULT);
-        */
     }
 
 
