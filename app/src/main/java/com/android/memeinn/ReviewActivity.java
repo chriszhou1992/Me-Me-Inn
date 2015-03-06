1cde27065dca19b423ea893797b1aa08dc8bb7e8
Chi Zhou <chriszhou1992@hotmail.com>
2015-03-06 16:11:28 -0600

    Review word list interface and backend finished - Chi

--- app/src/main/java/com/android/memeinn/ReviewActivity.java
+++ app/src/main/java/com/android/memeinn/ReviewActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -35,25 +37,43 @@ public class ReviewActivity extends Activity {
             public void done(List<ParseObject> results, ParseException e) {
                 if (e == null) {
                     TableLayout table = (TableLayout) findViewById(R.id.reviewTable);
+
+                    TableRow.LayoutParams wordColLayout = new TableRow.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+                    wordColLayout.span = 1;
+
+                    TableRow.LayoutParams definitionColLayout = new TableRow.LayoutParams(
+                            1,
+                            ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+                    definitionColLayout.span = 2;
+
                     for (ParseObject obj : results) {
                         TableRow tr = new TableRow(ReviewActivity.this);
 
                         TextView wordField = new TextView(ReviewActivity.this);
                         wordField.setText(obj.getString("word"));
-                        wordField.setLayoutParams(new TableRow.LayoutParams(
-                                ViewGroup.LayoutParams.WRAP_CONTENT,
-                                ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+                        wordField.setLayoutParams(wordColLayout);
                         wordField.setTextColor(Color.WHITE);
+                        wordField.setGravity(Gravity.START);
+                        wordField.setPadding(0,0,4,0);
 
                         TextView defField = new TextView(ReviewActivity.this);
                         defField.setText(obj.getString("definition"));
-                        defField.setLayoutParams(new TableRow.LayoutParams(
-                                0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+                        defField.setLayoutParams(definitionColLayout);
+                        defField.setGravity(Gravity.START);
                         defField.setTextColor(Color.WHITE);
+                        defField.setPadding(1,1,1,1);
 
                         tr.addView(wordField);
                         tr.addView(defField);
                         table.addView(tr);
+
+                        View separator = new View(ReviewActivity.this);
+                        separator.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup
+                                .LayoutParams.MATCH_PARENT, 2));
+                        separator.setBackgroundColor(Color.WHITE);
+                        table.addView(separator);
                     }
                 } else {
                     e.printStackTrace();
