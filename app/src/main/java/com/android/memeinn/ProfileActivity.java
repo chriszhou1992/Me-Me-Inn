438ee2ef107ac1c0276a967d1b88bf53f1afcfe3
Chi Zhou <chriszhou1992@hotmail.com>
2015-03-06 13:46:20 -0600

    Crude review backend finished - Chi

--- app/src/main/java/com/android/memeinn/ProfileActivity.java
+++ app/src/main/java/com/android/memeinn/ProfileActivity.java
@@ -3,20 +3,31 @@ package com.android.memeinn;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-        import com.parse.Parse;
+import com.parse.ParseUser;
 
-/**
- * Created by lindsey on 15-03-05.
- */
+
 public class ProfileActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        TextView userNameView = (TextView)findViewById(R.id.name);
+        ParseUser u = ParseUser.getCurrentUser();
+        userNameView.setText(u.getUsername());
+    }
+
+    /**
+     * Callback function for Review button. Triggers an Intent to go to the ReviewActivity.
+     * @param view The Button that is clicked.
+     */
+    public void gotoReview(View view) {
+        Intent reviewIntent = new Intent(this, ReviewActivity.class);
+        startActivity(reviewIntent);
     }
 
 }
--- app/src/main/java/com/android/memeinn/ReviewActivity.java
+++ app/src/main/java/com/android/memeinn/ReviewActivity.java
@@ -2,7 +2,63 @@ package com.android.memeinn;
 
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class ReviewActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.review);
+
+        Log.d("Myapp", "ReviewActivity");
+
+        ParseUser u = ParseUser.getCurrentUser();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("GRE");
+        query.whereEqualTo("reviewUsers", u);
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> results, ParseException e) {
+                if (e == null) {
+                    TableLayout table = (TableLayout) findViewById(R.id.reviewTable);
+                    for (ParseObject obj : results) {
+                        TableRow tr = new TableRow(ReviewActivity.this);
+
+                        TextView wordField = new TextView(ReviewActivity.this);
+                        wordField.setText(obj.getString("word"));
+                        wordField.setLayoutParams(new TableRow.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+                        wordField.setTextColor(Color.WHITE);
+
+                        TextView defField = new TextView(ReviewActivity.this);
+                        defField.setText(obj.getString("definition"));
+                        defField.setLayoutParams(new TableRow.LayoutParams(
+                                0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f));
+                        defField.setTextColor(Color.WHITE);
+
+                        tr.addView(wordField);
+                        tr.addView(defField);
+                        table.addView(tr);
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
