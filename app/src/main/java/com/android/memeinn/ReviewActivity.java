ba7e0b07a99479ddf4efe699e3ffa0676fbe8bf6
Lindsey Liu <jmjemi327@gmail.com>
2015-03-15 00:24:28 -0500

    implemented word review(haven't debugged yet)

--- app/src/main/java/com/android/memeinn/ReviewActivity.java
+++ app/src/main/java/com/android/memeinn/ReviewActivity.java
@@ -3,14 +3,10 @@ package com.android.memeinn;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
@@ -18,23 +14,35 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.ParseRelation;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReviewActivity extends Activity {
 
     private String vocabType = "";//the type of vocabulary
+    private ArrayList<ParseObject> wordList;
+    private int currPos;
+
+    private TextView wordContentView;
+    private TextView wordMeaningView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.review);
+        setContentView(R.layout.reviewchart);
 
         Intent intent = getIntent();
         vocabType = intent.getStringExtra(VocabActivity.EXTRA_MESSAGE);
 
-        Log.d("Myapp", "ReviewActivity");
+        Log.d("Myapp", "ReviewActivity.vocabType = " + vocabType);
 
+        initList();
+        currPos = 0;
+
+        /*
         ParseUser u = ParseUser.getCurrentUser();
         ParseQuery<ParseObject> query = ParseQuery.getQuery(vocabType);
         query.whereEqualTo("reviewUsers", u);
@@ -86,5 +94,89 @@ public class ReviewActivity extends Activity {
                 }
             }
         });
+        */
+    }
+
+    //disappear the circle and show word meaning hidden underneath
+    public void showMeaning(View view){
+        LinearLayout mLayout = (LinearLayout) view;
+        mLayout.setVisibility(View.GONE);
+    }
+
+    //user nailed the word
+    //remove the word from review list table
+    public void knowWord(View view){
+        ParseUser u = ParseUser.getCurrentUser();
+        String relationName = "reviewUsers"/*+vocabType*/;
+        ParseRelation relation = u.getRelation(relationName);
+        ParseObject word = wordList.get(currPos);
+        relation.remove(word);
+    }
+
+    public void onClickNext(View view) {
+        if (currPos < wordList.size()) {
+            currPos ++;
+            if (currPos == wordList.size()) {
+                currPos = 0;
+            }
+            updateMemorizationView();
+        }
+    }
+
+    /*
+    //control the Button Previous
+    public void onClickPrev(View view) {
+        if (currPos >= 0) {
+            currPos --;
+            if (currPos < 0) {
+                currPos = wordList.size() - 1;
+            }
+            updateMemorizationView();
+        }
+    }
+    */
+
+    //parse the vocabulary set and print the words and meanings
+    private void initList() {
+        ParseUser u = ParseUser.getCurrentUser();
+        String relationName = "reviewUsers"/*+vocabType*/;
+        ParseRelation relation = u.getRelation(relationName);
+        ParseQuery query = relation.getQuery();
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> wordList, ParseException e) {
+                if (e == null) {
+                    Log.d("MyApp", "get review word objects");
+                    initMemorizationView();
+                } else {
+                    Log.d("MyApp", "Error from retrieving review words: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    //initialize the memorization view page
+    private void initMemorizationView() {
+        setWordDisplayWithPos(0);
+    }
+
+    // helper function when click the next or previous button
+    private void updateMemorizationView() {
+        setWordDisplayWithPos(currPos);
+    }
+
+    //get the current word position to control the sequence
+    private void setWordDisplayWithPos(int pos) {
+        ParseObject word = wordList.get(pos);
+        String wordContent = word.getString("word");
+        String wordMeaning = word.getString("definition");
+        this.wordContentView.setText(wordContent);
+        this.wordMeaningView.setText(wordMeaning);
+    }
+
+    public void backToProfile(View view) {
+        Intent profIntent = new Intent(this, ProfileActivity.class);
+        startActivity(profIntent);
     }
 }
