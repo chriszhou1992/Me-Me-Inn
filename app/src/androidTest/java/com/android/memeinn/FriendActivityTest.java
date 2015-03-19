b6749befec8a0b8f95c11505d0ef6307de2940ca
qingsongqi <qi3031@hotmail.com>
2015-03-19 04:43:06 -0500

    Add FriendActivity test

---
+++ app/src/androidTest/java/com/android/memeinn/FriendActivityTest.java
@@ -0,0 +1,93 @@
+package com.android.memeinn;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.ViewInteraction;
+import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import org.hamcrest.Matcher;
+
+import java.util.List;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
+public class FriendActivityTest extends ActivityInstrumentationTestCase2<LoginActivity> {
+
+    public FriendActivityTest() {
+        super(LoginActivity.class);
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        // Espresso will not launch our activity for us, we must launch it via getActivity().
+        getActivity();
+    }
+
+    /**
+     * Friend name could be listed
+     */
+    public void testFriendList() {
+        onView(withId(R.id.uname)).perform(typeText("1111"), closeSoftKeyboard());
+        onView(withId(R.id.pword)).perform(typeText("1111"), closeSoftKeyboard());
+
+        //Test if correctly typed
+        onView(withId(R.id.uname)).check(matches(withText("1111")));
+        onView(withId(R.id.pword)).check(matches(withText("1111")));
+
+        //New intent to goto MainActivity
+        onView(withId(R.id.login)).perform(click());
+
+        //Check activity transition
+        onView(withId(R.id.vocab)).check(matches(withText("Go To Vocabulary")));
+        onView(withId(R.id.friends)).check(matches(withText("Match with Friends")));
+        onView(withId(R.id.ladder)).check(matches(withText("Match in Ladder")));
+
+        //New intent to goto ProfileActivity
+        onView(withId(R.id.imageButton)).perform(click());
+
+        //Check activity transition
+        onView(withId(R.id.friends)).check(matches(withText("Friends")));
+
+        //New intent to goto FriendActivity
+        onView(withId(R.id.friends)).perform(click());
+
+        //Check activity transition
+        onView(withId(R.id.name)).check(matches(withText("My Friends")));
+
+        ViewInteraction v = onView(withId(R.id.lst_users));
+        if (v == null)
+            Log.d("Friend List", "Friend List is null! ");
+        else
+            Log.d("Friend List", "Friend List is not null! ");
+
+        //New intent to goto FriendActivity
+        onView(withId(R.id.addfriend)).perform(click());
+
+        ViewInteraction w = onView(withId(R.id.txt_username));
+        if (w == null)
+            Log.d("User List", "User List is null! ");
+        else
+            Log.d("User List", "User List is not null! ");
+
+    }
+
+}
