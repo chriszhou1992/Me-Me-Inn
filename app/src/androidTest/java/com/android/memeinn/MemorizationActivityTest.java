56a661e9b6725652ccf04e8ee58634675d76cd89
wangeva <wangmifen@gmail.com>
2015-03-20 02:59:26 -0500

    Break down word frequency feature test version1

--- app/src/androidTest/java/com/android/memeinn/MemorizationActivityTest.java
+++ app/src/androidTest/java/com/android/memeinn/MemorizationActivityTest.java
@@ -1,11 +1,14 @@
 package com.android.memeinn;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -13,10 +16,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * Created by qingsongqi on 3/6/15.
  */
 
-public class MemorizationActivityTest extends ActivityInstrumentationTestCase2<ChapterActivity> {
+public class MemorizationActivityTest extends ActivityInstrumentationTestCase2<VocabActivity> {
 
     public MemorizationActivityTest() {
-        super(ChapterActivity.class);
+        super(VocabActivity.class);
     }
 
     @Override
@@ -26,13 +29,16 @@ public class MemorizationActivityTest extends ActivityInstrumentationTestCase2<C
         getActivity();
     }
 
+    
     public void testButtonA() {
+        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonA)).perform(click());
         onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
         onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
     }
 
     public void testButtonB() {
+        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonB)).perform(click());
         onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
         onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
@@ -40,15 +46,52 @@ public class MemorizationActivityTest extends ActivityInstrumentationTestCase2<C
 
 
     public void testButtonAIntent() {
+        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonA)).perform(click());
         onView(withId(R.id.wordContentView)).check(matches(isDisplayed()));
         onView(withId(R.id.wordMeaningView)).check(matches(isDisplayed()));
     }
 
     public void testButtonBIntent() {
+        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonA)).perform(click());
         onView(withId(R.id.wordContentView)).check(matches(isDisplayed()));
         onView(withId(R.id.wordMeaningView)).check(matches(isDisplayed()));
+    }
+
+    //newly added test cases
+
+    /**
+     * Test new frequency partitioning features
+     */
+
+
+    public void testWithHighFrequencyClicked() {
+        Log.d("Myapp", "testWithHighFrequencyClicked");
+        onView(withId(R.id.gre)).perform(click());
+        onView(withId(R.id.buttonA)).perform(click());
+
+        onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
+        onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
+    }
+
+    public void testWithMediumFrequencyClicked() {
+        Log.d("Myapp", "testWithMediumFrequencyClicked");
+        onView(withId(R.id.gre)).perform(click());
+        onView(withId(R.id.buttonB)).perform(click());
+
+        onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
+        onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
+    }
+
+
+    public void testWithLowFrequencyClicked() {
+        Log.d("Myapp", "testWithLowFrequencyClicked");
+        onView(withId(R.id.gre)).perform(click());
+        onView(withId(R.id.buttonC)).perform(click());
+
+        onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
+        onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
     }
 }
 
