9677f230d78b9711aace52aefcb0b4a00b5ed237
richardzhou1991 <szhou14@illinois.edu>
2015-05-03 16:05:53 -0500

    fixed test cases for chapter activity

--- app/src/androidTest/java/com/android/memeinn/ChapterActivityTest.java
+++ app/src/androidTest/java/com/android/memeinn/ChapterActivityTest.java
@@ -27,36 +27,26 @@ public class ChapterActivityTest extends ActivityInstrumentationTestCase2<Chapte
 
     public void testWithHighFrequencyClicked() {
         Log.d("Myapp", "testWithHighFrequencyClicked");
-        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonA)).perform(click());
-
+        onView(withId(R.id.chap1)).perform(click());
         onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
         onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
     }
 
     public void testWithMediumFrequencyClicked() {
         Log.d("Myapp", "testWithMediumFrequencyClicked");
-        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonB)).perform(click());
-
+        onView(withId(R.id.chap2)).perform(click());
         onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
         onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
     }
-
 
     public void testWithLowFrequencyClicked() {
         Log.d("Myapp", "testWithLowFrequencyClicked");
-        onView(withId(R.id.gre)).perform(click());
         onView(withId(R.id.buttonC)).perform(click());
-
+        onView(withId(R.id.chap3)).perform(click());
         onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
         onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
     }
 
-
-    public void testButtonIntent() {
-        onView(withId(R.id.buttonA)).perform(click());
-        onView(withId(R.id.prevWordBtn)).check(matches(withText("Previous")));
-        onView(withId(R.id.nextWordBtn)).check(matches(withText("Next")));
-    }
 }
--- app/src/androidTest/java/com/android/memeinn/FreqChapActivityTest.java
+++ app/src/androidTest/java/com/android/memeinn/FreqChapActivityTest.java
@@ -1,14 +1,12 @@
 package com.android.memeinn;
 
 import android.test.ActivityInstrumentationTestCase2;
-import android.util.Log;
 
 import com.android.memeinn.learn.VocabActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
